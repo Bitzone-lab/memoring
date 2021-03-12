809c0d26d5d9077fc9043fead213df619fe2caa2
ertrii <ertriilive@gmail.com>
2021-03-12 11:55:09 -0500

    feat: prefix in env

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,4 +1,4 @@
 export const BOT_TOKEN = process.env.BOT_TOKEN
-export const PREFIX = '!'
+export const PREFIX = process.env.PREFIX || '!'
 export const PORT: number = Number(process.env.PORT) || 8080
 export const MONGODB_URI = process.env.MONGODB_URI || ''
